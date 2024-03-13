6867a0302f741d687caa05666c44d2e7459601ce
Linus Johansson <golfarenlj@gmail.com>
2024-03-13 19:07:18 +0000

    Add code to calculate

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,24 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons){
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit"){
+                alert("You clicked Submit!")
+            } else{
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
 function rungame(){
 
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
+
 
 function checkAnswer(){
 
